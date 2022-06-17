986497da0adad13a3305e0409717ac92145e948d
RMOlive <rmolives@gmail.com>
2022-06-17 23:00:42 +0800

    call

--- src/main/kotlin/org/hime/Hime.kt
+++ src/main/kotlin/org/hime/Hime.kt
@@ -3,13 +3,19 @@ package org.hime
 import org.hime.core.SymbolTable
 import org.hime.core.core
 import org.hime.parse.*
+import java.io.File
+import java.nio.file.Files
 
-var symbolTable = SymbolTable(HashMap(), core)
+var defaultSymbolTable = SymbolTable(HashMap(), core)
 
-fun call(code: String): Token {
+fun call(code: String, symbolTable: SymbolTable = defaultSymbolTable): Token {
     val asts = parser(lexer(format(code)))
     var result = NIL
     for (ast in asts)
         result = org.hime.core.call(ast, symbolTable)
     return result
+}
+
+fun call(file: File, symbolTable: SymbolTable = defaultSymbolTable): Token {
+    return call(Files.readString(file.toPath()), symbolTable)
 }
