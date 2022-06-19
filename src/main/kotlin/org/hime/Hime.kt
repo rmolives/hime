b1cc327ce1660fcad76713c14ecc1867be5c8e36
RMOlive <rmolives@gmail.com>
2022-06-19 18:09:26 +0800

    eval

--- src/main/kotlin/org/hime/Hime.kt
+++ src/main/kotlin/org/hime/Hime.kt
@@ -4,8 +4,6 @@ import org.hime.core.SymbolTable
 import org.hime.core.core
 import org.hime.core.eval
 import org.hime.parse.*
-import java.io.File
-import java.nio.file.Files
 
 val defaultSymbolTable = SymbolTable(HashMap(), core)
 
@@ -15,8 +13,4 @@ fun call(code: String, symbolTable: SymbolTable = defaultSymbolTable): Token {
     for (ast in asts)
         result = eval(ast, symbolTable)
     return result
-}
-
-fun call(file: File, symbolTable: SymbolTable = defaultSymbolTable): Token {
-    return call(Files.readString(file.toPath()), symbolTable)
 }
