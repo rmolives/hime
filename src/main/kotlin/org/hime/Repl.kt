3761c5f86267e733516188261da3903e47cc8c8c
RMOLive <rmolives@gmail.com>
2022-07-08 00:19:20 +0800

    repl

--- src/main/kotlin/org/hime/Repl.kt
+++ src/main/kotlin/org/hime/Repl.kt
@@ -21,12 +21,11 @@ fun repl() {
             print("    ")
         var index = 0
         var flag = 0
-        val builder = StringBuilder()
         val read = reader.readLine()
         if (read.startsWith(":clear"))
             symbolTable = SymbolTable(HashMap(), core)
         else if (read.startsWith(":load"))
-            builder.append(Files.readString(Path.of(read.substring(6))))
+            codeBuilder.append(Files.readString(Path.of(read.substring(6))))
         else {
             codeBuilder.append(read)
             val code = codeBuilder.toString()
@@ -34,29 +33,20 @@ fun repl() {
                 ++index
             do {
                 if (code[index] == '\"') {
-                    builder.append("\"")
-                    val value = StringBuilder()
                     var skip = false
                     while (true) {
                         ++index
                         if (index < code.length - 1 && code[index] == '\\') {
-                            if (skip) {
-                                skip = false
-                                value.append("\\\\")
-                            } else
-                                skip = true
+                            skip = !skip
                             continue
                         } else if (index >= code.length - 1 || code[index] == '\"') {
                             if (skip) {
                                 skip = false
-                                value.append("\\\"")
                                 continue
                             } else
                                 break
                         }
-                        value.append(code[index])
                     }
-                    builder.append(value.append("\"").toString())
                     ++index
                     continue
                 }
@@ -64,12 +54,12 @@ fun repl() {
                     ++flag
                 else if (code[index] == ')')
                     --flag
-                builder.append(code[index++])
+                ++index
             } while (index < code.length)
         }
         if (flag == 0) {
+            val result = call(codeBuilder.toString(), symbolTable)
             codeBuilder = StringBuilder()
-            val result = call(builder.toString(), symbolTable)
             if (result.type != Type.NIL)
                 println(result.toString())
         }
