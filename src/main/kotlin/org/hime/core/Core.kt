98a48678cf28db5cce067e0b40ae5863d18504c2
RMOLive <rmolives@gmail.com>
2022-07-10 13:08:13 +0800

    nil

--- src/main/kotlin/org/hime/core/Core.kt
+++ src/main/kotlin/org/hime/core/Core.kt
@@ -383,6 +383,8 @@ val core = SymbolTable(
             assert(args.isNotEmpty())
             assert(args[0].type == LIST)
             val tokens = cast<List<Token>>(args[0].value)
+            if (tokens.size == 1)
+                return NIL
             val list = ArrayList<Token>()
             for (i in 1 until tokens.size)
                 list.add(tokens[i])
@@ -408,6 +410,8 @@ val core = SymbolTable(
             assert(args.isNotEmpty())
             assert(args[0].type == LIST)
             val tokens = cast<List<Token>>(args[0].value)
+            if (tokens.size == 1)
+                return NIL
             val list = ArrayList<Token>()
             for (i in 1 until tokens.size)
                 list.add(tokens[i])
