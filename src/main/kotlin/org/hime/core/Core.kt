0ff747bb67d1addcce86807a78991e41e5901ecb
RMOLive <rmolives@gmail.com>
2022-07-12 17:13:16 +0800

    token

--- src/main/kotlin/org/hime/core/Core.kt
+++ src/main/kotlin/org/hime/core/Core.kt
@@ -394,7 +394,7 @@ val core = SymbolTable(
             val newSymbol = symbol.createChild()
             for (node in ast.child) {
                 // 如果碰到else，就直接执行返回
-                if (node.tok.type == ID && cast<String>(node.tok.value) == "else")
+                if (node.tok.type == ID && node.tok.value.toString() == "else")
                     return eval(node[0].copy(), newSymbol)
                 else {
                     val result = eval(node[0].copy(), newSymbol)
@@ -409,12 +409,12 @@ val core = SymbolTable(
             assert(ast.size() > 1)
             val newSymbol = symbol.createChild()
             for(node in ast[1].child) {
-                if (node.tok.type == ID && cast<String>(node.tok.value) == "else")
+                if (node.tok.type == ID && node.tok.value.toString() == "else")
                     return eval(node[0].copy(), newSymbol)
                 else {
                     val result = eval(node[0].copy(), newSymbol)
                     assert(node.size() == 2)
-                    if (node.tok == ast[1].tok)
+                    if (node.tok == result)
                         return eval(node[1].copy(), newSymbol)
                 }
             }
