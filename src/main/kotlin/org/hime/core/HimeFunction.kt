5ed185973e7c3e0b25f43db662e05ab9d9c8b35d
RMOLive <rmolives@gmail.com>
2022-07-18 21:38:43 +0800

    type

--- src/main/kotlin/org/hime/core/HimeFunction.kt
+++ src/main/kotlin/org/hime/core/HimeFunction.kt
@@ -1,27 +1,27 @@
 package org.hime.core
 
-import org.hime.toToken
 import org.hime.cast
 import org.hime.parse.ASTNode
-import org.hime.parse.Type
 import org.hime.parse.Token
+import org.hime.parse.Type
+import org.hime.toToken
 
 typealias Hime_HimeFunction = (List<Token>) -> Token                        // 自举函数
 typealias Hime_Function = (List<Token>, SymbolTable) -> Token
 typealias Hime_StaticFunction = (ASTNode, SymbolTable) -> Token
 
 class HimeFunction(
-    val funcType: FuncType,
-    val func: Any,
-    val paramTypes: List<Type>,
-    val variadic: Boolean
+    private val funcType: FuncType,
+    private val func: Any,
+    private val paramTypes: List<Type>,
+    private val variadic: Boolean
 ) {
     // 接受任意类型，任意个数的参数的函数
     constructor(funcType: FuncType, func: Any) : this(funcType, func, listOf(), true)
 
     // 接受任意类型，指定个数的参数的函数
     constructor(funcType: FuncType, func: Any, size: Int) :
-            this(funcType, func, List(size, fun(_): Type { return Type.UNKNOWN }), false)
+            this(funcType, func, List(size, fun(_): Type = Type.UNKNOWN), false)
 
     fun call(ast: ASTNode, symbol: SymbolTable): Token {
         if (funcType == FuncType.STATIC) {
@@ -29,17 +29,15 @@ class HimeFunction(
             ast.clear()
             return ast.tok
         }
-
         for (i in 0 until ast.size())
             ast[i].tok = eval(ast[i].copy(), symbol.createChild())
         val args = ArrayList<Token>()
         for (i in 0 until ast.size())
             args.add(ast[i].tok)
-
         return call(args, symbol)
     }
 
-    fun call(args: List<Token>, symbol: SymbolTable): Token {
+    private fun call(args: List<Token>, symbol: SymbolTable): Token {
         assert(funcType != FuncType.STATIC)
 
         assert(args.size >= paramTypes.size) { "not enough arguments." }
@@ -61,7 +59,6 @@ class HimeFunction(
 
     fun call(args: List<Token>): Token {
         assert(funcType == FuncType.USER_DEFINED)
-
         return call(args, SymbolTable(HashMap(), null))
     }
 
