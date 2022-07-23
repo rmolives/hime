89de8dbbb92c507c14889a63a36c38838158d411
RMOLive <rmolives@gmail.com>
2022-07-24 04:05:38 +0800

    0.1

--- src/main/kotlin/org/hime/Main.kt
+++ src/main/kotlin/org/hime/Main.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
     if (args.isEmpty())
         repl()
     else if (args[0] == "--version" || args[0] == "-v")
-        println("Hime V0.1\nhttps://www.wumoe.org.cn/\nWuMoe Inc.")
+        println("Hime V0.1\nhttps://www.wumoe.org.cn/\nWuMoe Community.")
     else
         call(Files.readString(Path.of(args[0])))
 }
