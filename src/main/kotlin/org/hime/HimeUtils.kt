8c85dfe28c4251a8ee8071e1551d4679edb885bc
RMOLive <rmolives@gmail.com>
2022-08-02 16:41:05 +0800

    type

--- src/main/kotlin/org/hime/HimeUtils.kt
+++ src/main/kotlin/org/hime/HimeUtils.kt
@@ -1,9 +1,8 @@
 package org.hime
 
 fun generateRandomString(len: Int = 16): String{
-    val alphanumerics = CharArray(26) { it -> (it + 97).toChar() }.toSet()
-        .union(CharArray(9) { it -> (it + 48).toChar() }.toSet())
     return (0 until len).map {
-        alphanumerics.toList().random()
+        CharArray(26) { (it + 97).toChar() }.toSet()
+            .union(CharArray(9) { (it + 48).toChar() }.toSet()).toList().random()
     }.joinToString("")
 }
