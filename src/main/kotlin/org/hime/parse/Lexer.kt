18b49a9fd57fab7ed5a4363aa68982720084a977
RMOLive <rmolives@gmail.com>
2022-08-06 02:22:55 +0800

    type

--- src/main/kotlin/org/hime/parse/Lexer.kt
+++ src/main/kotlin/org/hime/parse/Lexer.kt
@@ -8,15 +8,11 @@ import java.math.BigInteger
 import java.math.MathContext
 
 /**
- * 词法分析器
- * @param env   环境
+ * 分割表达式
  * @param code  代码
- * @return      一系列Token List
+ * @return      expressions
  */
-fun lexer(env: Env, code: String): List<List<Token>> {
-    // 返回的内容
-    val result = ArrayList<List<Token>>()
-    // 当前计算的内容
+fun splitCode(code: String): List<String> {
     val expressions = ArrayList<String>()
     var index = 0
     while (index < code.length) {
@@ -70,129 +66,135 @@ fun lexer(env: Env, code: String): List<List<Token>> {
         } while (flag > 0)
         expressions.add(builder.toString())
     }
-    // 分析一个表达式
-    for (expressionIndex in expressions.indices) {
-        // 存储当前表达式中的Token
-        val tokens = ArrayList<Token>()
-        result.add(tokens)
-        val expression = expressions[expressionIndex]
-        // 考虑边界条件
-        index = -1
-        // 开始扫描...
-        while (++index < expression.length) {
-            when (expression[index]) {
-                '(' -> {
-                    tokens.add(env.himeLb)
-                    continue
-                }
-                ')' -> {
-                    tokens.add(env.himeRb)
-                    continue
-                }
-                // 跳过空格
-                ' ' -> continue
-            }
-            var negative = false
-            // 处理负数
-            if (expression[index] == '-' && index < expression.length - 1 && Character.isDigit(expression[index + 1])) {
-                negative = true
-                ++index
-            }
-            // 处理小数.
-            if (Character.isDigit(expression[index])) {
-                var v = BigInteger.ZERO
-                while (true) {
-                    if (index >= expression.length - 1 || !Character.isDigit(expression[index])) {
-                        --index
-                        break
-                    }
-                    v = v.multiply(BigInteger.valueOf(10))
-                        .add(BigInteger.valueOf((expression[index].digitToIntOrNull() ?: -1).toLong()))
-                    ++index
-                }
-                // 处理浮点数
-                if (expression[index + 1] != '.') {
-                    tokens.add((if (negative) v.subtract(v.multiply(BigInteger.TWO)) else v).toToken(env))
-                    continue
-                }
-                var x = BigDecimal(v.toString())
-                var d = BigDecimal.valueOf(10)
-                ++index
-                while (true) {
-                    ++index
-                    if (index >= expression.length - 1 || !Character.isDigit(expression[index])) {
-                        --index
-                        break
-                    }
-                    x = x.add(
-                        BigDecimal.valueOf((expression[index].digitToIntOrNull() ?: -1).toLong())
-                            .divide(d, MathContext.DECIMAL64)
-                    )
-                    d = d.multiply(BigDecimal.valueOf(10))
-                }
-                tokens.add((if (negative) x.subtract(x.multiply(BigDecimal.valueOf(2))) else x).toToken(env))
+    return expressions
+}
+
+/**
+ * 词法分析器
+ * @param env           环境
+ * @param expression    代码
+ * @return              tokens
+ */
+fun lexer(env: Env, expression: String): MutableList<Token> {
+    // 返回的内容
+    val tokens = ArrayList<Token>()
+    // 考虑边界条件
+    var index = -1
+    // 开始扫描...
+    while (++index < expression.length) {
+        when (expression[index]) {
+            '(' -> {
+                tokens.add(env.himeLb)
                 continue
             }
-            // 处理字符串
-            if (expression[index] == '\"') {
-                val builder = StringBuilder()
-                var skip = false
-                while (true) {
-                    ++index
-                    if (index < expression.length - 1 && expression[index] == '\\') {
-                        if (skip) {
-                            skip = false
-                            builder.append("\\")
-                        } else
-                            skip = true
-                        continue
-                    } else if (index >= expression.length - 1 || expression[index] == '\"') {
-                        if (skip) {
-                            skip = false
-                            builder.append("\"")
-                            continue
-                        } else
-                            break
-                    }
+
+            ')' -> {
+                tokens.add(env.himeRb)
+                continue
+            }
+            // 跳过空格
+            ' ' -> continue
+        }
+        var negative = false
+        // 处理负数
+        if (expression[index] == '-' && index < expression.length - 1 && Character.isDigit(expression[index + 1])) {
+            negative = true
+            ++index
+        }
+        // 处理小数.
+        if (Character.isDigit(expression[index])) {
+            var v = BigInteger.ZERO
+            while (true) {
+                if (index >= expression.length - 1 || !Character.isDigit(expression[index])) {
+                    --index
+                    break
+                }
+                v = v.multiply(BigInteger.valueOf(10))
+                    .add(BigInteger.valueOf((expression[index].digitToIntOrNull() ?: -1).toLong()))
+                ++index
+            }
+            // 处理浮点数
+            if (expression[index + 1] != '.') {
+                tokens.add((if (negative) v.subtract(v.multiply(BigInteger.TWO)) else v).toToken(env))
+                continue
+            }
+            var x = BigDecimal(v.toString())
+            var d = BigDecimal.valueOf(10)
+            ++index
+            while (true) {
+                ++index
+                if (index >= expression.length - 1 || !Character.isDigit(expression[index])) {
+                    --index
+                    break
+                }
+                x = x.add(
+                    BigDecimal.valueOf((expression[index].digitToIntOrNull() ?: -1).toLong())
+                        .divide(d, MathContext.DECIMAL64)
+                )
+                d = d.multiply(BigDecimal.valueOf(10))
+            }
+            tokens.add((if (negative) x.subtract(x.multiply(BigDecimal.valueOf(2))) else x).toToken(env))
+            continue
+        }
+        // 处理字符串
+        if (expression[index] == '\"') {
+            val builder = StringBuilder()
+            var skip = false
+            while (true) {
+                ++index
+                if (index < expression.length - 1 && expression[index] == '\\') {
                     if (skip) {
                         skip = false
-                        // 替换特殊值
-                        builder.append(
-                            when (expression[index]) {
-                                'n' -> "\n"
-                                'r' -> "\r"
-                                't' -> "\t"
-                                'b' -> "\b"
-                                else -> expression[index]
-                            }
-                        )
+                        builder.append("\\")
                     } else
-                        builder.append(expression[index])
-                }
-                tokens.add(builder.toString().toToken(env))
-                continue
-            }
-            // 处理其他字符
-            if (expression[index] != ' ' && expression[index] != '(' && expression[index] != ')') {
-                val builder = StringBuilder()
-                // 其他字符通常形成一个ID
-                while (true) {
-                    if (index >= expression.length - 1 || expression[index] == ' ' || expression[index] == ')') {
-                        --index
+                        skip = true
+                    continue
+                } else if (index >= expression.length - 1 || expression[index] == '\"') {
+                    if (skip) {
+                        skip = false
+                        builder.append("\"")
+                        continue
+                    } else
                         break
-                    }
-                    builder.append(expression[index])
-                    ++index
                 }
-                val s = builder.toString()
-                if (s.contains(":")) {
-                    val inOf = s.indexOf(":")
-                    tokens.add(Token(HimeTypeId(env, env.getType(s.substring(inOf + 1))), s.substring(0, inOf)))
+                if (skip) {
+                    skip = false
+                    // 替换特殊值
+                    builder.append(
+                        when (expression[index]) {
+                            'n' -> "\n"
+                            'r' -> "\r"
+                            't' -> "\t"
+                            'b' -> "\b"
+                            else -> expression[index]
+                        }
+                    )
                 } else
-                    tokens.add(Token(HimeTypeId(env), builder.toString()))
-                continue
+                    builder.append(expression[index])
             }
+            tokens.add(builder.toString().toToken(env))
+            continue
+        }
+        // 处理其他字符
+        if (expression[index] != ' ' && expression[index] != '(' && expression[index] != ')') {
+            val builder = StringBuilder()
+            // 其他字符通常形成一个ID
+            while (true) {
+                if (index >= expression.length - 1 || expression[index] == ' ' || expression[index] == ')') {
+                    --index
+                    break
+                }
+                builder.append(expression[index])
+                ++index
+            }
+            val s = builder.toString()
+            if (s.contains(":")) {
+                val inOf = s.indexOf(":")
+                tokens.add(Token(HimeTypeId(env, env.getType(s.substring(inOf + 1))), s.substring(0, inOf)))
+            } else
+                tokens.add(Token(HimeTypeId(env), builder.toString()))
+            continue
         }
     }
-    return result
+    return tokens
 }
