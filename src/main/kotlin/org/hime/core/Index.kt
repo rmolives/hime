874ef081667eba90bee1ffead2dcc469cff9a7fd
RMOLive <rmolives@gmail.com>
2022-08-06 16:43:53 +0800

    del regex

--- src/main/kotlin/org/hime/core/Index.kt
+++ src/main/kotlin/org/hime/core/Index.kt
@@ -4,6 +4,5 @@ val module = mutableMapOf(
     "util.file" to ::initFile,
     "util.time" to ::initTime,
     "util.table" to ::initTable,
-    "util.thread" to ::initThread,
-    "util.regex" to ::initRegex
+    "util.thread" to ::initThread
 )
